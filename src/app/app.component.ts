0c5c5de32011bd4627d89f2e43d2d2ac49e0afb6
ramnd <ramndiaz@gmail.com>
2021-02-17 22:13:07 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('estado', [
+      state('inactivo', style({
+        backgroundColor: 'black',
+        transform: 'scale(1)',
+        color: 'blue'
+      })),
+      state('activo', style({
+        backgroundColor: 'blue',
+        transform: 'scale(2.0)',
+        color: 'black'
+      })),
+      transition('inactivo => activo', animate('1000ms ease-in')),
+      transition('activo => inactivo', animate('1000ms ease-out'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'anima';
+  estado: string = 'inactivo';
+  cambioEstado(){
+    this.estado =(this.estado=='activo') ? 'inactivo':'activo';
+  }
 }
